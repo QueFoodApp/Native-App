2c19015e0e37a4fafdfc2f29c828de436dbe00bb
adarwis7 <90807429+adarwis7@users.noreply.github.com>
2025-01-10 12:31:45 -0500

    Refactor JWT configuration to remove default values for SECRET_KEY and ACCESS_TOKEN_EXPIRE_MINUTES

--- QueFood/backend/app/api/auth.py
+++ QueFood/backend/app/api/auth.py
@@ -10,9 +10,9 @@ from fastapi import HTTPException
 load_dotenv()
 
 # JWT configuration
-SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")  # Set secret key in .env
+SECRET_KEY = os.getenv("SECRET_KEY")  # Set secret key in .env
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 300000))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 # Password hashing functions
 def hash_password(password: str) -> str:
