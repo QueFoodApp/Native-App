bfb78f055922f4d1853dc2e961b80d7615a087f7
adarwis7 <90807429+adarwis7@users.noreply.github.com>
2025-03-12 18:03:52 -0400

    Fix formatting of API_BASE_URL in configuration file

--- QueFood/config.js
+++ QueFood/config.js
@@ -1,4 +1,4 @@
- const API_BASE_URL = "http://localhost:8000"; 
+const API_BASE_URL = "http://localhost:8000"; 
 // const API_BASE_URL = "http://192.168.68.53:8000"; 
 export default API_BASE_URL;
 
